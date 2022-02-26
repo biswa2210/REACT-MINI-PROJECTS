68877123db0539acf7ebbca22bed6d10bb7711c4
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2022-02-26 20:59:51 +0530

    CREATED BY BISWA

--- src/events.js
+++ src/events.js
@@ -1,6 +1,11 @@
 export const eventHandling=() => {
   alert("Aukaat ke baahar");
 }
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 export const eventHandlingApple = () => {
   alert("Aukaat ke baahar,Apne pajame main rahe k baat kar");
 }
